5f72345da329465c973c6132b997ba3ec34d2452
guoxudong <88137956@qq.com>
2019-11-29 08:20:36 +0000

    travis automated update

--- kubecm.rb
+++ kubecm.rb
@@ -1,9 +1,9 @@
 class Kubecm < Formula
   desc "Merge multiple kubeconfig"
   homepage "https://github.com/sunny0826/kubecm"
-  url "https://github.com/sunny0826/kubecm/releases/download/v0.4.1/kubecm_0.4.1_Darwin_x86_64.tar.gz"
-  version "v0.4.1"
-  sha256 "5e12cbc13994d5b6deaf08db198ce04ba332d0b20d06b20462e072e5be8d4fc9"
+  url "https://github.com/sunny0826/kubecm/releases/download/v0.5.0/kubecm_0.5.0_Darwin_x86_64.tar.gz"
+  version "v0.5.0"
+  sha256 "bb216f36676e802af728d4f8facd56fecb6eddfa7913c0368ab55a21eae574d8"
 
   def install
     bin.install "kubecm"
