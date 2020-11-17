4157d5006b1c1066b6d2da0ee9900c33f0341349
goreleaserbot <goreleaser@carlosbecker.com>
2020-11-17 16:56:14 +0800

    Brew formula update for kubecm version v0.10.2

--- kubecm.rb
+++ kubecm.rb
@@ -7,18 +7,25 @@ class Kubecm < Formula
 
   if OS.mac?
     url "https://github.com/sunny0826/kubecm/releases/download/v0.10.2/kubecm_0.10.2_Darwin_x86_64.tar.gz"
-    sha256 "305138010a8b1ac4864637ea6b742f039ac95c7e94bc84efee908404a4b075b8"
+    sha256 "01d67b3b6fdf349e30e75a43e40153e2b2e92f68c1301ad0483eba465a7ac582"
   end
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/sunny0826/kubecm/releases/download/v0.10.2/kubecm_0.10.2_Linux_x86_64.tar.gz"
-    sha256 "2918dcf4fb1187a05b022ecf03754e1563d22ef8e30c847085da4218747a566b"
+    sha256 "4a0cf5b5cb47cdac57ae22528b3850a90cf8d9909b5560c243bd3a414be1438e"
   end
   
   depends_on "git"
-  depends_on "go"
 
   def install
     bin.install "kubecm"
+    
+    # Install bash completion
+    output = Utils.safe_popen_read("#{bin}/kubecm", "completion", "bash")
+    (bash_completion/"kubecm").write output
+    
+    # Install zsh completion
+    output = Utils.safe_popen_read("#{bin}/kubecm", "completion", "zsh")
+    (zsh_completion/"_kubecm").write output
   end
 
   test do
