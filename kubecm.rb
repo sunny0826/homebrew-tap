4f1d2584b5343f45466dd6d099101ea5b74e34a9
goreleaserbot <goreleaser@carlosbecker.com>
2021-02-07 10:27:41 +0800

    Brew formula update for kubecm version v0.15.2

--- kubecm.rb
+++ kubecm.rb
@@ -5,20 +5,20 @@
 class Kubecm < Formula
   desc "Merge multiple kubeconfig"
   homepage "https://github.com/sunny0826/kubecm"
-  version "0.15.1"
+  version "0.15.2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/sunny0826/kubecm/releases/download/v0.15.1/kubecm_0.15.1_Darwin_x86_64.tar.gz"
-    sha256 "04fe5e5519995efe6962a6271085101b497a428796350099409c0c156881fb91"
+    url "https://github.com/sunny0826/kubecm/releases/download/v0.15.2/kubecm_0.15.2_Darwin_x86_64.tar.gz"
+    sha256 "6371968014f6e9bf2f604f3763e52bec8ceeaa6487985556e73c28ee790cb89b"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/sunny0826/kubecm/releases/download/v0.15.1/kubecm_0.15.1_Linux_x86_64.tar.gz"
-    sha256 "6b9e24295fd9aa4c7fb61da653532d19590f8baa96df7b104942564c3397e7f9"
+    url "https://github.com/sunny0826/kubecm/releases/download/v0.15.2/kubecm_0.15.2_Linux_x86_64.tar.gz"
+    sha256 "cb9b7fb8be8e4e9eebc74a488c92073ccbb9cc5cbdfe9a443ef79a84f3886ddb"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/sunny0826/kubecm/releases/download/v0.15.1/kubecm_0.15.1_Linux_arm64.tar.gz"
-    sha256 "a8edebb73a62db5f9861674f16c9338db6b48ee43be94d4458aea1ef62357e16"
+    url "https://github.com/sunny0826/kubecm/releases/download/v0.15.2/kubecm_0.15.2_Linux_arm64.tar.gz"
+    sha256 "9a66f4a3c3e3cbc81de59e0b4e6bdc96fb200e75ae3cbf7706f02f0143ea75a1"
   end
 
   depends_on "git"
